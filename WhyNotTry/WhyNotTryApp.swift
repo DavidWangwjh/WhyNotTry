b341535c9f8e2908b1310b133d17812f9731bc8c
DavidWangwjh <davidwang.wjh@gmail.com>
2025-01-20 14:30:15 -0800

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by David Wang on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhyNotTryTests/WhyNotTryTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryTests.swift
+//  WhyNotTryTests
+//
+//  Created by David Wang on 1/20/25.
+//
+
+import Testing
+@testable import WhyNotTry
+
+struct WhyNotTryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
